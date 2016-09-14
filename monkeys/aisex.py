81deaf59fea86b9ab9da4de44ef0a27a30750d4b
zhaozhou <zhaozhou@hikerlink.org>
2016-09-15 00:17:34 +0800

    userscript

--- monkeys/aisex.py
+++ monkeys/aisex.py
@@ -10,7 +10,7 @@ baseurl = 'http://bt.aisex.com/bt/'
 
 
 def lemons():
-    aisex = (baseurl + 'thread.php?fid=4&search=&page=' + str(i) for i in range(1, 0, -1))
+    aisex = (baseurl + 'thread.php?fid=4&search=&page=' + str(i) for i in range(10, 0, -1))
     for url in aisex:
         r = requests.get(url)
         soup = BeautifulSoup(r.text, "html.parser")
@@ -26,16 +26,8 @@ def harvest():
         soup = BeautifulSoup(r.text, "html.parser")
         tags = soup.find_all('a', href=re.compile("www.jandown.com"))
         for tag in tags:
-            print(tag['href'])
+            spawn(tag['href'])
             break
-                #     for juice in juices:
-                #         try:
-                #             juice = Juice(key_name=lemon, image=juice['image'], download=juice['download'])
-                #             juice.put()
-                #         except BadValueError:
-                #             logging.info(juice)
-                #     bucket.append(lemon)
-                # squeezed.put()
 
 
 if __name__ == "__main__":
