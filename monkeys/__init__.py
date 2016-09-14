0d71094a060cf057f4398d22210d9119d00c21e9
zhaozhou <zhaozhou@hikerlink.org>
2016-09-14 12:14:37 +0800

    userscript

--- monkeys/__init__.py
+++ monkeys/__init__.py
@@ -8,6 +8,7 @@ import dropbox
 from bencodepy import decode, encode
 from urllib.parse import urlparse
 import re
+import json
 
 browser = mechanicalsoup.Browser()
 
@@ -30,7 +31,7 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 def spawn_torrent(url):
     torrent = r.hgetall(url)
     if torrent:
-        return torrent
+        return json.loads(torrent)
     else:
         torrent_url_components = urlparse(url)
         torrent_url_query = torrent_url_components.query
@@ -51,7 +52,7 @@ def spawn_torrent(url):
                 btih = hashlib.sha1(encode(info)).hexdigest()
                 dn = metainfo[b'info'][b'name']
                 magnet = 'magnet:?xt=urn:btih:{btih}&dn={dn}'.format(btih=btih, dn=dn)
-                torrent = {"status": "OK", "magnet": magnet, "torrent": link}
+                torrent = {'status': 'OK', 'magnet': magnet, 'torrent': link}
                 r.hmset(url, torrent)
                 return torrent
             except:
