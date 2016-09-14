4731575063d1170f8bd12246b060c594a5d7fe76
zhaozhou <zhaozhou@hikerlink.org>
2016-09-14 12:16:05 +0800

    userscript

--- monkeys/__init__.py
+++ monkeys/__init__.py
@@ -31,7 +31,7 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 def spawn_torrent(url):
     torrent = r.hgetall(url)
     if torrent:
-        return json.loads(torrent)
+        return json.loads(json.dumps(torrent))
     else:
         torrent_url_components = urlparse(url)
         torrent_url_query = torrent_url_components.query
