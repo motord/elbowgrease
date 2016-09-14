6cc803690e3ef7b393ce7e65773c1c04670d53e2
zhaozhou <zhaozhou@hikerlink.org>
2016-09-14 23:07:39 +0800

    userscript

--- monkeys/aisex.py
+++ monkeys/aisex.py
@@ -3,8 +3,7 @@ __author__ = 'peter'
 
 import requests
 from bs4 import BeautifulSoup
-from .cheekpouch import spawn
-import logging
+from cheekpouch import spawn
 
 baseurl = 'http://bt.aisex.com/bt/'
 
@@ -13,16 +12,16 @@ def lemons():
     aisex = (baseurl + 'thread.php?fid=4&search=&page=' + str(i) for i in range(1, 0, -1))
     for url in aisex:
         r=requests.get(url)
-        soup=BeautifulSoup(r.text)
+        soup=BeautifulSoup(r.text, "html.parser")
         tags=soup.find_all('a', target="_blank")
         for tag in tags:
-            logging.info(tag)
+            print(tag)
             yield tag
 
 
 def harvest():
     for lemon in lemons():
-        logging.info(lemon)
+        print(lemon)
     #     for juice in juices:
     #         try:
     #             juice = Juice(key_name=lemon, image=juice['image'], download=juice['download'])
