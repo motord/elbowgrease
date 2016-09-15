cfb4a17f05872690505fc329cf08e01dbdb3622b
zhaozhou <zhaozhou@hikerlink.org>
2016-09-15 23:36:04 +0800

    axel userscript

--- grease/axel.user.js
+++ grease/axel.user.js
@@ -6,14 +6,15 @@
 // @name                Axel Episodes
 // @namespace	        http://v.numag.net/grease/
 // @description	        grab all episodes for axel download
-// @include		http://hyper.numag.net/*/*
-// @include		http://amsterdam.hikerlink.org/youtube/*/*
+// @include		http://hyper.numag.net/*/
+// @include		http://amsterdam.hikerlink.org/youtube/*/
 // ==/UserScript==
 (function () {
     var elmNewContent = document.createElement('div');
 
     elmNewContent.textContent = $x("/html/body/pre/a/@href").slice(1).map(function (element) {
         return 'x256 ' + window.location.href + element.value;
-    }).join([separator = '; '])
+    }).join([separator = '; ']);
+    elmNewContent.setAttribute("style", "color: #f8f8f8; font-family: 'Raleway',sans-serif; font-size: 18px; font-weight: 500; line-height: 32px; margin: 0 0 24px; ");
     document.body.appendChild(elmNewContent);
 })();
